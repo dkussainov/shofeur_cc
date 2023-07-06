960abafb6e8b0ccd5349f6551fd3572e8d588595
Dulat <dulat.kussainov@gmail.com>
2023-07-06 07:16:21 -0400

    Added events to the page

--- components/Event.jsx
+++ components/Event.jsx
@@ -5,13 +5,13 @@ const Event = ({ event }) => {
     <div class="prompt_card">
       <Image
         class="w-full rounded-lg drop-shadow-md"
-        src={event?.image}
+        src={event.image}
         alt="Sunset in the mountains"
         width={300}
         height={300}
       />
       <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2 underline">{event?.title}</div>
+        <div class="font-bold text-xl mb-2 underline">{event.title}</div>
       </div>
     </div>
   );
